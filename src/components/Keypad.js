32d3155c0d18eba506fae33d7635a6becd1ec6e0
Wafaa.Alhayek <wafash.alhayek@gmail.com>
2023-01-28 20:24:33 +0200

    done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,12 @@
 // Code Keypad Component Here
+import React from "react";
+
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input type="password" onChange={() => {console.log("Entering password...")}} />
+        </div>
     )
 }
 
